50cab7efa46af724d09c1a201ae87c3d975616e0
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-08-24 11:39:02 -0400

    feat: add 9.17 solution

---
+++ ch9/917.c
@@ -0,0 +1,88 @@
+/* next_fit allocator */
+/* Showing only changes and additions relative to mm.c. */
+/* Only the mm_init, coalesce and find_fit functions  */
+/* need to be modified. */
+
+/* Global variables */
+static char *last_fit;           /* Used by next fit */
+
+int mm_init(void) 
+{
+    /* Create the initial empty heap */
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1) //line:vm:mm:begininit
+        return -1;
+    PUT(heap_listp, 0);                          /* Alignment padding */
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); /* Prologue header */ 
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1)); /* Prologue footer */ 
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));     /* Epilogue header */
+    heap_listp += (2*WSIZE);                     //line:vm:mm:endinit  
+    /* $end mminit */
+
+    /* Initialize last fit at prologue */
+    last_fit = heap_listp;
+
+    /* Extend the empty heap with a free block of CHUNKSIZE bytes */
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL) 
+        return -1;
+    return 0;
+}
+/* $end mminit */
+
+/*
+ * coalesce - Boundary tag coalescing. Return ptr to coalesced block
+ */
+static void *coalesce(void *bp) 
+{
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc) {            /* Case 1 */
+        return bp;
+    }
+
+    else if (prev_alloc && !next_alloc) {      /* Case 2 */
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size,0));
+    }
+
+    else if (!prev_alloc && next_alloc) {      /* Case 3 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+    else {                                     /* Case 4 */
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + 
+            GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    /* $end mmfree */
+
+    /* Make last_fit pointer isn't part of a coalesced block */
+    if ((last_fit > (char *)bp) && (last_fit < NEXT_BLKP(bp))) 
+        last_fit = bp;
+
+
+    return bp;
+}
+
+
+static void *find_fit(size_t asize) {
+  void *bp;
+
+  for (bp = last_fit; bp != last_fit; bp = NEXT_BLKP(bp)) {
+    /* Go back to prologue when reaching epilogue */
+    if (GET_SIZE(HDRP(bp)) == 0) 
+      bp = heap_listp;
+    else if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+      last_fit = bp;
+      return bp;
+    }
+  }
+  return NULL;
+}
