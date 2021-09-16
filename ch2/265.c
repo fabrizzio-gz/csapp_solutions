04c64729163ac1f53b742e76db968e87931bb2fd
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-09-16 12:53:11 -0400

    chore: change folder names, delete files

--- ch2/265.c
+++
@@ -1,6 +0,0 @@
-/* Return 1 when x contains an odd number of 1s; 0 otherwise.
-   Assume w=32 */
-
-int odd_ones(unsigned x) {
-  
-}
--- ch2/273.c
+++
@@ -1,8 +0,0 @@
-#include <limits.h>
-
-/* Addition that saturates to TMin or TMax */
-int saturating_add(int x, int y) {
-  x + y
-  int overflow = INT_MAX - x - y; 
-  x + y 
-}
